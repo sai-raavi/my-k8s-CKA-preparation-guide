108aa3d8f878c7ee51a1cc65a730aee17af0cb22
Sainath Raavi <raavis1112@gmail.com>
2025-08-13 21:46:04 -0700

    Update cp_setup.sh

--- setup-guides/cp_setup.sh
+++ setup-guides/cp_setup.sh
@@ -3,9 +3,8 @@
 set -e
 
 ip_addr=$(hostname -i)
-
-new_host_entries="${ip_addr} $(hostname)
-${ip_addr} cp"
+pub_ip_addr=$(curl ifconfig.me)
+new_host_entries="${pub_ip_addr} $(hostname)"
 
 echo "Adding the following entries to /etc/hosts:"
 echo "$new_host_entries" | cat - /etc/hosts | sudo tee /etc/hosts > /dev/null
